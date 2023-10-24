49086cafa3a8660ea617949556f5a8067a816edd
0oshowero0 <o0shower0o@outlook.com>
2023-10-24 19:37:34 +0800

    bug fix

--- generate_city_boundary.py
+++ generate_city_boundary.py
@@ -47,7 +47,7 @@ for _, row in tqdm(esri_worldcity.iterrows(), total=esri_worldcity.shape[0]):
 
 
 
-all_city_gpd = gpd.GeoDataFrame({'CITY_NAME': all_city_city_name_list, 'ADMIN_NAME': all_city_province_name_list, 
+all_city_gpd = gpd.GeoDataFrame({'CityName': all_city_city_name_list, 'StateName': all_city_province_name_list, 
                                  'CountryCode': all_city_country_code, 'CountryName': all_city_country_name,    
                                  'CityStatus': all_city_city_status, 'geometry': all_city_geometry_list, 
                                  'Population': all_city_city_pop, 'PopulationClass': all_city_city_pop_class,
@@ -62,7 +62,7 @@ gdp = pd.read_csv('./Data/World_Bank/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_5871885/AP
 gdp = gdp.dropna()
 # 读取世界银行数据：GDP Country Info
 country_info = pd.read_csv('./Data/World_Bank/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_5871885/Metadata_Country_API_NY.GDP.MKTP.CD_DS2_en_csv_v2_5871885.csv')[['Country Code', 'Region', 'IncomeGroup']].rename(columns={'Country Code':'CountryCode'})
-gdp = gdp.merge(country_info, on='CountryCode', how='left')
+gdp = gdp.merge(country_info, on='CountryCode', how='outer')
 
 # 读取世界银行数据：Per Capita GDP
 percapitagdp = pd.read_csv('./Data/World_Bank/API_NY.GDP.PCAP.CD_DS2_en_csv_v2_5871588/API_NY.GDP.PCAP.CD_DS2_en_csv_v2_5871588.csv')[['Country Name','Country Code','2020']].rename(columns={'Country Name':'CountryName','Country Code':'CountryCode','2020':'PerCapitaGDP'})
@@ -71,17 +71,28 @@ percapitagdp = percapitagdp.dropna()
 country_info = pd.read_csv('./Data/World_Bank/API_NY.GDP.PCAP.CD_DS2_en_csv_v2_5871588/Metadata_Country_API_NY.GDP.PCAP.CD_DS2_en_csv_v2_5871588.csv')[['Country Code', 'Region', 'IncomeGroup']].rename(columns={'Country Code':'CountryCode'})
 percapitagdp = percapitagdp.merge(country_info, on='CountryCode', how='left')
 
-all_gdp = gdp.merge(percapitagdp, on=['CountryCode','CountryName','Region','IncomeGroup'], how='left')
+all_gdp = gdp.merge(percapitagdp, on=['CountryCode','CountryName','Region','IncomeGroup'], how='outer')
 
 
 # 世界银行采用3位国家编码，而Esri WorldCity采用2位国家编码，需要转换
-country_code_lut = pd.read_csv('country_code_lut.csv')[['Alpha2','Alpha3']]
-#all_city_gpd = selected_city
-all_city_gpd = all_city_gpd.merge(country_code_lut, left_on='CountryCode', right_on='Alpha2', how='left')
+country_code_lut = pd.read_csv('./Data/country_code_lut.csv')[['Alpha2Mod','Alpha3']]
+all_city_gpd = all_city_gpd.merge(country_code_lut, left_on='CountryCode', right_on='Alpha2Mod', how='left')
 
 # 与gdp数据合并
-all_city_gpd = all_city_gpd.drop(['CountryCode', 'CountryName'], axis=1).merge(all_gdp, left_on='Alpha3', right_on='CountryCode', how='left')
-all_city_gpd = all_city_gpd.drop(['Alpha2','area'], axis=1).rename(columns={'CITY_NAME':'CityName', 'ADMIN_NAME':'StateName'})
+all_city_gpdA = all_city_gpd.drop(['CountryCode', 'CountryName'], axis=1).merge(all_gdp, left_on='Alpha3', right_on='CountryCode', how='inner')
+all_city_gpdB = all_city_gpd.drop(['CountryCode'], axis=1).merge(all_gdp, left_on='CountryName', right_on='CountryName', how='inner')
+
+# 将两个结果合并
+all_city_gpd_merge = pd.concat([all_city_gpdA, all_city_gpdB]).drop_duplicates(subset=['ID'], keep='last').reset_index(drop=True)
+# 比较和all_city_gpd的差异，找到没有匹配上的城市；手动调整
+# all_city_gpd['match'] = all_city_gpd['ID'].isin(all_city_gpd_merge['ID'])
+# all_city_gpd_no_match = all_city_gpd.loc[all_city_gpd['match']==False].reset_index(drop=True)
+# print(all_city_gpd_no_match.drop_duplicates(subset=['CountryName'], keep='last').reset_index(drop=True))
+
+all_city_gpd_merge = all_city_gpd_merge.drop(['Alpha2Mod','Alpha3','area'], axis=1).sort_values(by=['ID']).reset_index(drop=True)
+all_city_gpd = all_city_gpd_merge
+
+
 
 # 为了测量面积，可以重新投射到Lambert Cylindrical投影（epsg:6933）
 # 兰伯特圆柱投影是一种保留面积测量的等面积投影。在地球表面具有相同大小的区域在地图上具有相同的大小。然而，形状、角度和比例尺一般会被扭曲。
---
+++ generate_city_center_for_arcgis.py
@@ -0,0 +1,6 @@
+import geopandas as gpd
+
+selected_city = gpd.read_parquet('./city_boundary.parquet')
+selected_city['geometry'] = selected_city['geometry'].centroid
+
+selected_city.to_file('./city_center.shp', driver='ESRI Shapefile')
